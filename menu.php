e10bdf08aa17db3cf0c51e0e78881fc1355c5526
Franklin13620 <franklintorres13620@gmail.com>
2020-04-19 10:49:51 -0600

    nuevas estiquetas

--- menu.php
+++ menu.php
@@ -3,7 +3,10 @@
     <div class="col">
         <ul class="nav justify-content-center">
             <li class="nav-item">
-                <a class="nav-link active" href="datosAlumno.php">Estudiantes</a>
+                <a class="nav-link" href="inicio.php">Inicio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="datosAlumno.php">Estudiantes</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Profesores</a>
@@ -16,6 +19,12 @@
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Horarios</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="usuarios.php">Usuarios</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="inicioSesion.php">Sesi&oacute;n</a>
             </li>
         </ul>
     </div>
