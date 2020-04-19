a1e445036a9d7716a46710894c92e58ba039c64a
Franklin13620 <franklintorres13620@gmail.com>
2020-04-18 23:40:54 -0600

    base

---
+++ cabecera.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+ <!-- Required meta tags -->
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ <title>Diseño con Bootstrap</title>
+ <link rel="stylesheet" href="css/bootstrap.min.css">
+ <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <header>
+                    <img src="../img/banner.png" class="img-fluid" alt="">
+                </header>
+            </div>
+        </div>
+
---
+++ menu.php
@@ -0,0 +1,22 @@
+<!-- menu principal -->
+<div class="row negro mt-3">
+    <div class="col">
+        <ul class="nav justify-content-center">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Estudiantes</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Profesores</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Carreras</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Materias</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Horarios</a>
+            </li>
+        </ul>
+    </div>
+</div>
