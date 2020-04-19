edb1778958183eaafef08079b365213c3ee3d0ca
Franklin13620 <franklintorres13620@gmail.com>
2020-04-19 11:49:09 -0600

    port postgresql

--- conectar.php
+++ conectar.php
@@ -4,7 +4,7 @@ $password = "f8b0fe36a6edab402cb901587b411213cc79482005acf0034bce2cb529b16ad1";
 $dbname = "ddp7f2ofc7vt8i";
 
 try {
-	$conn = new PDO('mysql:host=ec2-18-206-84-251.compute-1.amazonaws.com;dbname='.$dbname, $usuario, $password);
+	$conn = new PDO('mysql:host=ec2-18-206-84-251.compute-1.amazonaws.com;port=5432;dbname='.$dbname, $usuario, $password);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	// echo "Conexion satisfactoria";
 } catch(PDOException $e){
