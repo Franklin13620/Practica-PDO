e31a248b043b0fcaed27b7031d631780370505ef
Franklin13620 <franklintorres13620@gmail.com>
2020-04-18 21:22:25 -0600

    Navbar

---
+++ inicio.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>PDO</title>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="css/estilos.css" rel="stylesheet">  
+</head>
+<body>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <header>
+        </header>
+        </div>
+      </div>
+    </div>
+  <!-- menu principal -->
+    <div class="row">
+      <div class="col">
+        <ul class="nav justify-content-center negro">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Registro de Estudiantes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Registro de Profesores</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Registro de Carreras</a>
+          </li>
+          <li class="nav-item">
+          <a class="nav-link" href="#">Registro de Materias</a>
+          </li>
+          <li>
+          <a class="nav-link active" href="#">Registro de Horarios</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+        <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="../js/jquery-3.4.1.slim.min.js"></script>
+    <script src="../js/popper.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+  </body>
+</html>
